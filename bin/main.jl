d9ee05af7240ea61a898848e73cb30d66f8e2c24
Anshul Singhvi <anshulsinghvi@gmail.com>
2025-02-26 14:34:48 -0500

    Add comments and log statements
    to more precisely know what is going on

--- bin/main.jl
+++ bin/main.jl
@@ -1,28 +1,79 @@
+#=
+This is a simple test case and example of the 
+single-producer, multiple-session architecture with Bonito.
+
+There is a global Observable that 
+=#
+
 using Bonito, Observables, WGLMakie
 # using Bonito: @js_str, onjs, Button, TextField, Slider, linkjs, Session, App
 using Bonito.DOM
 # using Hyperscript
+@info "Loaded all packages"
 
 color = Observable("red")
+number_of_listeners = 0
 
 Threads.@spawn while true
     sleep(1)
     color[] = color[] == "red" ? "blue" : "red"
+    # Log when number of listeners changes - this is important to know
+    # so that we can understand if the Observable architecture used here makes
+    # sense, or if we should use a different architecture, like channels or 
+    # global arrays that get polled every frame.
+    if number_of_listeners != length(color.listeners)
+        number_of_listeners = length(color.listeners)
+        @info "Number of listeners changed to $number_of_listeners"
+    end
 end
+@info "Instantiated async task to change color every second"
 
-
+# Create a Bonito app for each session,
+# that creates a plot where the color is linked to the 
+# global `color` Observable.  This is a proxy for any
+# global task that runs once but must be displayed in 
+# multiple sessions.
 app = App() do session::Session
+    @info "Creating app for session "
     f, a, p = lines(rand(10); color = color)
     return f
 end
 
+# If the server was already created, close it
+# Mainly for interactive use in the REPL
 if isdefined(Main, :server)
     close(server)
 end
 
-server = Bonito.Server(app, "$(get(ENV, "BONITO_DNS", "trybonito")).apps.internal.juliahub.com", parse(Int, get(ENV, "BONITO_PORT", "8081")))
+# Get the DNS and port from environment variables,
+# and log the values we are using.
+dns = get(ENV, "BONITO_DNS", "trybonito")
+port = get(ENV, "BONITO_PORT", "8081")
+if haskey(ENV, "BONITO_DNS")
+    @info "Using DNS from environment variable BONITO_DNS: $dns"
+else
+    @info "Using default DNS: $dns"
+end
+
+if haskey(ENV, "BONITO_PORT")
+    @info "Using port from environment variable BONITO_PORT: $port"
+else
+    @info "Using default port: $port"
+end
+
+# Construct the Bonito server
+@info "Constructing Bonito server on $(dns).apps.internal.juliahub.com:$port"
+server = Bonito.Server(app, "$(dns).apps.internal.juliahub.com", parse(Int, port))
 # Important Note: You might want to set the keyword argument `proxy_url` above in case
 # you have a reverse proxy (like nginx or caddy) in front of the Bonito instance.
+
+# Start the server
+@info "Starting Bonito server"
 Bonito.HTTPServer.start(server)
 # Bonito.HTTPServer.route!(server, "/" => app) # Overwrite app after changing it
+@info "Server successfully started, waiting on connections"
+
+# Wait for the server to exit, because if running in an app, the app will
+# exit when the script is done.  This makes sure that the app is only closed
+# if (a) the server closes, or (b) the app itself times out and is killed externally.
 wait(server)
